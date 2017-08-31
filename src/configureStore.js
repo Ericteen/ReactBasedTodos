c9b09e862ab1c820f748d6b3eaf0cd7dcbc92863
Ericteen <chengteen@yeah.net>
2017-08-31 18:20:48 +0800

    dispatch log

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,24 @@
 import { createStore } from 'redux'
 import { throttle } from 'lodash'
 import todoApp from './reducers'
-import { loadState, saveState } from './utils/localStorage.js'
+import { loadState, saveState } from './utils/localStorage'
+
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch
+  if (!console.group) {
+    return rawDispatch
+  }
+
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
 
 const configureStore = () => {
   const persistedState = loadState()
@@ -9,6 +26,13 @@ const configureStore = () => {
       todoApp,
       persistedState
     )
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
+
+  store.dispatch = addLoggingToDispatch(store)
+
   store.subscribe(throttle(() => {
     saveState({
       todos: store.getState().todos
