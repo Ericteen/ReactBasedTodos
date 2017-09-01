32bc4e3417407e5d26cd585c7d5e2de08b030e61
Ericteen <chengteen@yeah.net>
2017-09-01 14:32:52 +0800

    displaying loading indicator

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 import * as actions from '../actions'
 import TodoList from '../components/TodoList'
-import { getVisibleTodos } from '../reducers'
+import { getVisibleTodos, getIsFetching } from '../reducers'
 import React, { Component } from 'react';
 
 export class VisibleTodoList extends Component {
@@ -16,15 +16,21 @@ export class VisibleTodoList extends Component {
   }
 
   fetchData() {
-    const { filter, fetchTodos } = this.props
+    const { filter, requestTodos, fetchTodos } = this.props
+    requestTodos(filter)
     fetchTodos(filter)
   }
 
   render() {
-    const { toggleTodo, ...rest } = this.props
+    const { toggleTodo, todos, isFetching } = this.props
+
+    if (isFetching && !todos.length) {
+      return (<p>Loading..</p>)
+    }
+
     return (
       <TodoList
-       {...rest} 
+       todos={todos} 
        onTodoClick={toggleTodo}
       />
     )
@@ -35,6 +41,7 @@ const mapStateToProps = (state, ownProps) => {
   const filter = ownProps.filter
   return {
     todos: getVisibleTodos(state, filter),
+    isFetching: getIsFetching(state, filter),
     filter,
   }
   
