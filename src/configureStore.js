baf79d61be21d0f13f7e7d59cc660c2235561a3d
Ericteen <chengteen@yeah.net>
2017-12-04 18:40:07 +0800

    优化

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,18 +1,20 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import {createLogger} from 'redux-logger'
 import todoApp from './reducers'
 import thunk from 'redux-thunk'
 
 const configureStore = () => {
   const middlewares = [thunk]
-
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger())
   }
 
   return createStore(
     todoApp,
-    applyMiddleware(...middlewares)
+    composeEnhancers(
+      applyMiddleware(...middlewares)
+    )
   )
 }
 
